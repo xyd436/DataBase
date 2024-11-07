42a3e2a3c6420f04c2664b8105213a23d36b569c
Asteria-JX <1523648892@qq.com>
2024-11-07 17:46:35 +0800

    实现路由

--- dual_selection_platform/src/main.js
+++ dual_selection_platform/src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { router } from './router'; // 导入路由配置
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router); // 使用路由中间件
+
+app.mount('#app'); // 挂载 Vue 应用到 DOM 元素中
