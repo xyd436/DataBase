ff696ed570fb9072b53d6ff05c9ee6162287c154
Haanyu-yan <2051856548@qq.com>
2024-11-09 14:48:58 +0800

    spring boot后端

---
+++ src/test/java/com/example/dual_selection_platform_back/DualSelectionPlatformBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dual_selection_platform_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualSelectionPlatformBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
