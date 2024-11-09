b3f2027b0b78a89587df29559a0e34fed2477e23
Haanyu-yan <2051856548@qq.com>
2024-11-09 16:35:53 +0800

    spring boot后端

---
+++ back_dual_selection_platform/src/main/java/com/example/back_dual_selection_platform/BackDualSelectionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.back_dual_selection_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackDualSelectionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackDualSelectionPlatformApplication.class, args);
+    }
+
+}
---
+++ back_dual_selection_platform/src/test/java/com/example/back_dual_selection_platform/BackDualSelectionPlatformApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.back_dual_selection_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDualSelectionPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
