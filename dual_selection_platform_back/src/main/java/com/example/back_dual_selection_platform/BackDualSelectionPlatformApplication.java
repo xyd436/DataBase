605609a8f97e72d9bf5096daa9b5ed5b1f3783a0
Haanyu-yan <2051856548@qq.com>
2024-11-10 16:48:16 +0800

    model完成

--- dual_selection_platform_back/src/main/java/com/example/back_dual_selection_platform/BackDualSelectionPlatformApplication.java
+++ dual_selection_platform_back/src/main/java/com/example/back_dual_selection_platform/BackDualSelectionPlatformApplication.java
@@ -3,11 +3,12 @@ package com.example.back_dual_selection_platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Spring boot运行需要启动这里的main方法
 @SpringBootApplication
 public class BackDualSelectionPlatformApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackDualSelectionPlatformApplication.class, args);
     }
-
 }
---
+++ dual_selection_platform_back/src/main/java/com/example/back_dual_selection_platform/model/User.java
@@ -0,0 +1,24 @@
+package com.example.back_dual_selection_platform.model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@lombok.Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel("用户信息")
+
+public class User {
+    @ApiModelProperty(value = "用户ID", required = true)
+    private int id;
+
+    @ApiModelProperty(value = "用户名", required = true)
+    private String username;
+
+    @ApiModelProperty(value = "密码", required = true)
+    private String password;
+
+    @ApiModelProperty(value = "角色ID", required = true)
+    private String role_id;
+}
